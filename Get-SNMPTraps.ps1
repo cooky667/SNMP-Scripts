a37684952176b8d9f7ca487a11049da7be8c6dd6
cooky667 <cooky667@hotmail.com>
2017-04-21 13:21:40 +0100

    Initial Scripts

---
+++ Get-SNMPTraps.ps1
@@ -0,0 +1,95 @@
+﻿Function Get-SNMPTraps($ComputerName="localhost", $CommunityString="B00m3r!") {
+
+$results = @()
+$SessionOption = New-CimSessionOption -Protocol Dcom
+
+    ForEach($Computer in $ComputerName) {
+
+        if (!(Test-Connection -count 1 $Computer -ErrorAction SilentlyContinue)) {
+
+            Write-Host "$Computer is not available" -ForegroundColor Red
+            continue
+
+        }
+    
+    try {
+
+        $session = New-CimSession -ComputerName $Computer -SessionOption $SessionOption -ErrorAction Stop
+
+        }
+
+    catch {
+
+        Write-Host "There was an unknown error connecting to WMI on $computer, skipping this one" -ForegroundColor Red
+        Remove-Variable SNMPTrapDestination, SNMPCommunityString, SNMPTRAPServiceStatus, SNMPServiceStatus, Hostname, reg, regkey -ErrorAction SilentlyContinue
+        continue
+
+        }
+
+
+    Write-Host "Processing $Computer..." -ForegroundColor Green
+   
+    if ((Get-CimInstance -CimSession $session win32_service | Where-Object {$_.Name -eq "SNMP"}) -or (Get-CimInstance -CimSession $session win32_service | Where-Object {$_.Name -eq "SNMPTRAP"})) {
+        
+       $Hostname = (Get-CimInstance -CimSession $session win32_computersystem -Property name).name
+       $SNMPServiceStatus = (Get-CimInstance -CimSession $session win32_service | Where-Object {$_.Name -eq "SNMP"}).state
+       $SNMPTRAPServiceStatus = (Get-CimInstance -CimSession $session win32_service | Where-Object {$_.Name -eq "SNMPTRAP"}).state
+
+    }
+
+    else {
+
+        $SNMPServiceStatus = "not installed"
+        $SNMPTRAPServiceStatus = "not installed"
+
+    }
+
+    Remove-CimSession -CimSession $session
+
+        try {
+
+            $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $Hostname)
+            $regKey= $reg.OpenSubKey("SYSTEM\CurrentControlSet\Services\SNMP\Parameters\TrapConfiguration\$CommunityString")
+
+        }
+
+    catch {
+
+        Write-Host "There was an unknown error connecting to the registry on $computer, skipping this one" -ForegroundColor Red
+        Remove-Variable SNMPTrapDestination, SNMPCommunityString, SNMPTRAPServiceStatus, SNMPServiceStatus, Hostname, reg, regkey -ErrorAction SilentlyContinue
+        continue
+
+        }
+
+
+    if ($regKey -eq $null){
+    
+    $SNMPCommunityString = "not valid"
+    
+    }
+    
+    else{
+    
+    $SNMPCommunityString = $CommunityString
+    $SNMPTrapDestination = $regkey.getvalue($regkey.getvaluenames()[0])
+    } 
+
+    $Parameters = @{
+
+    ComputerName = $Computer
+    SNMPSvcStatus = $SNMPServiceStatus
+    SNMPTrapSvcStatus = $SNMPTRAPServiceStatus
+    SNMPTrapDestination = $SNMPTrapDestination
+    SNMPTrapCommunityString = $SNMPCommunityString
+    
+    }
+
+    $results += New-Object -TypeName psobject -Property $Parameters
+
+    Remove-Variable SNMPTrapDestination, SNMPCommunityString, SNMPTRAPServiceStatus, SNMPServiceStatus, Hostname, reg, regkey -ErrorAction SilentlyContinue
+
+    }
+
+    return $results
+
+}
---
+++ Set-SNMPTraps.ps1
@@ -0,0 +1,78 @@
+﻿Function Set-SNMPTraps($ComputerName="localhost", $CommunityString, $SNMPTrapDestination, $RestartService=$False, $AutoStart=$True) {
+
+    $SessionOption = New-CimSessionOption -Protocol Dcom
+
+    ForEach($Computer in $ComputerName) {
+
+        if (!(Test-Connection -count 1 $Computer -ErrorAction SilentlyContinue)) {
+
+            Write-Host "$Computer is not available" -ForegroundColor Red
+            continue
+
+        }
+    
+        try {
+
+            $session = New-CimSession -ComputerName $Computer -SessionOption $SessionOption -ErrorAction Stop
+
+        }
+
+        catch {
+
+            Write-Host "There was an unknown error connecting to WMI on $computer, skipping this one" -ForegroundColor Red
+            Remove-Variable SNMPCommunityString, SNMPTRAPServiceStatus, SNMPServiceStatus, Hostname, reg, regkey -ErrorAction SilentlyContinue
+            continue
+
+        }
+
+
+        Write-Host "Processing $Computer..." -ForegroundColor Green
+   
+        $Hostname = (Get-CimInstance -CimSession $session win32_computersystem -Property name).name
+
+
+        
+
+        try {
+
+            $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $Hostname)
+            $regkey = $reg.CreateSubkey("SYSTEM\CurrentControlSet\Services\SNMP\Parameters\TrapConfiguration\$CommunityString")
+            $regkey.SetValue("1", $SNMPTrapDestination,"String")
+            $regkey.Close()
+
+        }
+
+        catch {
+
+            Write-Host "There was an error when setting the reg key on $computer, skipping this one" -ForegroundColor Red
+            Remove-Variable SNMPCommunityString, SNMPTRAPServiceStatus, SNMPServiceStatus, Hostname, reg, regkey -ErrorAction SilentlyContinue
+            continue
+
+        }
+
+        if ($RestartService) {
+
+        
+            Get-CimInstance -CimSession $session win32_service | where-object {$_.name -eq "SNMPTRAP"} | Invoke-CimMethod -MethodName StopService
+
+            while ((Get-CimInstance -CimSession $session win32_service | where-object {$_.name -eq "SNMPTRAP"}).state -ne "stopped") {
+
+                Start-Sleep -Seconds 1
+
+            }
+
+            Get-CimInstance -CimSession $session win32_service | where-object {$_.name -eq "SNMPTRAP"} | Invoke-CimMethod -MethodName StartService    
+
+        }
+
+        if ($AutoStart) {
+
+            Get-CimInstance -CimSession $session win32_service | where-object {$_.name -eq "SNMPTRAP"} | Invoke-CimMethod -MethodName ChangeStartMode -Arguments @{startmode='automatic'}
+
+        }
+
+        Remove-CimSession -CimSession $session
+    }
+
+  
+}
